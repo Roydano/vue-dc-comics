37a95d1113ba87d75cd77b554baa2e5220d23fd5
Roydano <roy.dano90@gmail.com>
2021-07-07 17:42:50 +0200

    add impostazione card series

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,14 +1,40 @@
 <template>
   <section>
-      <div class="container">
-          <h3>Content goes here</h3>
+      <div class="jumbotron"></div>
+
+      <div class="btn-series">
+           <button>current series</button>
       </div>
+
+      <div class="container flex">  
+          <div v-for="(card, index) in cards" :key="index" class="card-container">
+              <DcCard  :link="card"/>
+          </div>
+      </div>
+
+      
+      <div class="btn">
+          <button>load more</button>
+      </div>
+
   </section>
 </template>
 
 <script>
+import DcCard from '@/components/DcCard.vue';
+import CardSeries from '@/data/CardSeries.js';
+
 export default {
     name: 'Content',
+    components: {
+        DcCard
+    },
+
+    data(){
+        return{
+            cards: CardSeries,
+        }
+    }
 }
 </script>
 
@@ -17,12 +43,35 @@ export default {
 
     section{
         background-color: $black-ground;
-        height: 80px;
-    }
 
-    h3{
-        color: white;
-        line-height: 80px;
+        .jumbotron{
+            width: 100%;
+            height: 400px;
+            background-image: url(../assets/img/jumbotron.jpg);
+            background-size: cover;
+        }
+
+        .flex{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        .card-container{
+            padding: 20px;
+            width: calc((100% / 6) * 1);
+
+            img{
+                width: 100%;
+            }
+
+        }
+
+        .btn{
+            display: flex;
+            justify-content: center;
+            padding: 15px;
+        }
     }
 
 </style>
